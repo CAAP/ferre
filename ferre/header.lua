bb70ea03c3c65efbb9b25291eb689d308d9d23f2
Charlie Root <root@puffy.localdomain>
2016-05-12 00:54:21 -0500

    Fixing some changes

---
+++ ferre/header.lua
@@ -0,0 +1,16 @@
+#!/bin/ferre/lua
+
+local sql = require'carlos.sqlite'
+local fd = require'carlos.fold'
+
+local conn = sql.connect'/db/ferre.sql'
+
+local datos = conn.header'datos'
+assert(datos, 'Table datos not found in /db/ferre.sql')
+datos[#datos] = nil -- remove fecha
+
+local function quot(x) return string.format('%q', x) end
+
+local head = table.concat(fd.reduce( datos, fd.map( quot ), fd.into, {} ), ',')
+
+print(string.format('Content-Type: text/html\r\n\r\n[%s]\n', head))
