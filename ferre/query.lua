310d2f56d271ddb88569cb80d36c839b9c4ed542
Charlie Root <root@caap.localdomain>
2016-07-24 09:14:40 -0500

    Initial Query SQL

---
+++ ferre/query.lua
@@ -0,0 +1,19 @@
+#!/usr/local/bin/lua
+
+local aux = require'ferre.aux'
+local json = require'ferre.json'
+
+local function record( q )
+    local tbname = 'datos'
+    local clause = string.format("WHERE desc LIKE %q", q.desc)
+    local w = {	tbname= tbname,
+		dbname= '/db/ferre.sql',
+		clause= clause,
+		QRY= string.format('SELECT * FROM %q %s LIMIT 1', tbname, clause) }
+    return json( w )
+end
+
+aux( record )
+
+
+
