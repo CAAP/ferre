8c1662cf7ef91cf87d35f7edf324b55a0d5529e5
Charlie Root <root@caap.localdomain>
2016-07-25 13:43:26 -0500

    NE

--- ferre/query.lua
+++ ferre/query.lua
@@ -5,7 +5,7 @@ local json = require'ferre.json'
 
 local function record( q )
     local tbname = 'datos'
-    local clause = string.format("WHERE desc LIKE %q", q.desc)
+    local clause = string.format("WHERE desc LIKE %q ORDER BY desc", q.desc:gsub('*','%%')..'%%')
     local w = {	tbname= tbname,
 		dbname= '/db/ferre.sql',
 		clause= clause,
