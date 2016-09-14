289bd4091139fa2468822c66a18d2241268575eb
Charlie Root <root@ferreBSD.bancomer.com>
2016-09-13 19:52:39 -0500

    UFFF

--- caja/print.lua
+++ caja/print.lua
@@ -5,19 +5,20 @@ local lpr = require'ferre.lpr'
 local tkt = require'ferre.ticket'
 
 local fd = require'carlos.fold'
+local fs = require'carlos.files'
 
 local function tickets( q )
-    local tag = q.tag
-    local person = q.person
+--    local tag = q.tag
+--    local person = q.person
 
 -- maybe change to use fd.reduce || another version in stream.lua exists
     local function collect( a )
 	local t = {}
-	for k,v in a.gmatch'([^%s]+)%s([^%s]+)' do t[k] = v end
+	for k,v in a:gmatch'([^%s]+)%s([^%s]+)' do t[k] = v end
 	return t
     end
 
-    q.data = fd.reduce( q.args, collect, fd.into, {} )
+    q.data = fd.reduce( q.args, fd.map(collect), fd.into, {} )
 
     lpr( tkt( q ) )
 
