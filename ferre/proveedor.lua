6d0af2679559c23701c868bfee2817ac8d9bfa91
Charlie Root <root@ferreb.rga.ip>
2016-10-04 11:28:53 -0500

    Good

--- ferre/proveedor.lua
+++ ferre/proveedor.lua
@@ -8,19 +8,23 @@ local aux = require'ferre.aux'
 
 local function vals(s)
      local c,p = s:match('clave|([^|]+)|proveedor|([^|]+)')
-     return c and {string.format('%q', c), string.format('%q', (p or ''))} or false
+     return c and {clave=c, proveedor=(p or '')} or false
 end
 
 local function records( q )
-    local stmt = 'CREATE TABLE IF NOT EXISTS proveedores (clave PRIMARY KEY, proveedor)'
+--    local stmt = 'CREATE TABLE IF NOT EXISTS proveedores (clave PRIMARY KEY, proveedor)'
     local conn = sql.connect'/db/inventario.db'
 
-    assert( conn.exec'DROP TABLE IF EXISTS proveedores' )
-    assert( conn.exec(stmt), string.format('Error executing: %s', stmt) )
+--    assert( conn.exec'DROP TABLE IF EXISTS proveedores' )
+--    assert( conn.exec(stmt), string.format('Error executing: %s', stmt) )
 
-    assert( #q.args > 0, 'No data received!' )
+--    assert( #q.args > 0, 'No data received!' )
+    local upds = fd.reduce( q.args, fd.map(function(s) return vals(s) end), fd.filter(function(x) return x end), fd.into, {} )
 
-    fd.reduce( q.args, fd.map(function(s) return vals(s) end), fd.filter(function(x) return x end), sql.into'proveedores', conn)
+    fd.reduce( upds, function(w) assert(conn.exec(string.format('UPDATE proveedores SET proveedor = %q WHERE clave LIKE %q', w.proveedor, w.clave))) end )
+--    fs.dump('/cgi-bin/ferre/ferre/myfiles.txt', table.concat(upds, '\n'))
+
+--    fd.reduce( q.args, fd.map(function(s) return vals(s) end), fd.filter(function(x) return x end), sql.into'proveedores', conn)
 
     return 'OK'
 end
--- ticket/print.lua
+++ ticket/print.lua
@@ -1,30 +1,57 @@
-    local function collect( q )
-	local t = { '', '' } -- uid & id_tag
-	for k,v in q:gmatch'([^|]+)|([^|]+)' do if keys[k] then t[keys[k]] = v end end
-	return t
-    end
+#!/usr/local/bin/lua
 
-    local function ids( uid, tag ) return fd.map( function(t) t[1] = uid; t[2] = tag; return t end ) end
+local aux = require'ferre.aux'
+local lpr = require'ferre.bixolon'
+local tkt = require'ferre.ticket'
 
-    local function subTotal( w )
-	w.subTotal = string.format( '%.2f', w.totalCents / 100 )
+local fd = require'carlos.fold'
+local fs = require'carlos.files'
+local sql = require'carlos.sqlite'
+
+local function int(d) return math.tointeger(d) or d end
+
+local function tickets(q)
+    local uid = q.uid
+    local y,m,d = uid:match'^(%d+)-(%d+)-(%d+)'
+    local week = os.date( 'W%W', os.time{year=y, month=m, day=d} )
+    local conn = assert(sql.connect(string.format('/db/%s.db', week)), 'Error while connecting to DB ' .. week)
+    local QRY = 'SELECT * FROM tickets WHERE uid LIKE %q'
+    local PRC = 'SELECT desc, precio%d ||"/"|| IFNULL(u%d,"?") prc FROM precios WHERE clave LIKE %q'
+
+    assert(conn.exec'ATTACH DATABASE "/db/ferre.db" AS FR')
+
+    local nombres = fd.reduce( conn.query'SELECT * FROM empleados', fd.rejig(function(w) return w.nombre, w.id end), fd.merge, {} )
+
+    local function precio(w)
+	local j = w.precio:sub(-1)
+	w.clave = int(w.clave)
+	w.qty = int(w.qty)
+	w.rea = int(w.rea)
+	local ret = fd.first( conn.query(string.format(PRC, j, j, w.clave)), function(x) return x end )
+	w.desc = ret.desc
+	w.prc = ret.prc
+	w.subTotal = string.format('%.2f', w.totalCents/100)
 	return w
     end
 
-    local function asTable( s )
-	local t = {}
-	for k,v in s:gmatch'([^|]+)|([^|]+)' do t[k] = v end
-	return t
+    local function fetch()
+	local fecha = uid:match'([^P]+)P'
+	local p = nombres[int(uid:match'(%d+)$')] or 'NaP'
+
+	local ret = {fecha=fecha, person=p, tag=''}
+
+        local total = 0
+	local suma = fd.map(function(w) total = total + w.totalCents; return w end)
+
+	ret.datos = fd.reduce( conn.query(string.format(QRY, uid)), suma, fd.map(precio), fd.into, {} )
+	ret.total = string.format('%.2f', total/100)
+
+	return ret
     end
 
-    local tkt = require'ferre.ticket'
-    local lpr = require'ferre.bixolon'
-    local function forPrinting( w )
-	w.total = string.format( '%.2f', w.totalCents / 100 )
-	w.fecha = w.uid:match'([^P]+)P'
-	w.datos = fd.reduce( w.args, fd.map( asTable ), fd.map( MM.tickets.addDescPrc ), fd.map( subTotal ), fd.into, {} )
-        lpr( tkt( w ) )
-	w.datos = nil
-    end
+    lpr( tkt( fetch() ) )
 
+    return 'OK'
+end
 
+aux( tickets )
