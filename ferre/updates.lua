9f92fc20b4a7b64bd1b65bfd56c502d224c762a2
Charlie Root <root@ferreb.rga.ip>
2017-01-06 17:48:39 -0600

    UPDATES

--- ferre/updates.lua
+++ ferre/updates.lua
@@ -4,51 +4,47 @@ local sql = require'carlos.sqlite'
 local fd = require'carlos.fold'
 
 local aux = require'ferre.aux'
-local mx = reequire'ferre.timezone'
+local mx = require'ferre.timezone'
+local hd = require'ferre.header'
 
 local week = os.date('W%U', mx())
 
-local function quot(x)
-    if 'table' == type(x) then return string.format('[%s]', table.concat(fd.reduce(x, fd.map(quot), fd.into, {}), ', ')) end
-    return tonumber(x) and (math.tointeger(x) or x) or string.format('%q', x)
-end 
-
-local JSON = { 'clave', 'desc', 'fecha', 'faltante', 'obs', 'version', 'precio1', 'u1', 'precio2', 'u2', 'precio3', 'u3' }
-
-local function tovec(a)
-    local ret = fd.reduce( JSON, fd.map(function(k) return quot(a[k] or '') end), fd.into, {} )
-    return string.format('[%s]', table.concat(ret, ', '))
-end
-
-
-local stores
-
-local function events(k, v)
-end
-
 local QRY = 'SELECT * FROM updates %s'
 
-local function push(conn, clause, ret)
-    fd.reduce(conn.qry(string.format(QRY, clause)), fd.into, ret)
+local function push(week, vers, ret)
+    local conn = assert( sql.connect(string.format('/db/%s.db', week)) )
+    local clause = string.format('WHERE vers > %d', vers)
+
+    local function into(w)
+	local clave = w.clave
+	if not ret[clave] then ret[clave] = {clave=clave, store='PRICE'} end
+	local z = ret[clave]
+	z[w.campo] = w.valor
+    end
+
+    if conn.count('updates', clause) > 0 then
+	fd.reduce(conn.query(string.format(QRY, clause)), into)
+    end
+
+    return conn.count'updates'
 end
 
 local function records(w)
     local ret = {}
-    local clause = 'WHERE vers > %d'
     local vers = w.vers
+
     if w.week < week then -- XXX should NOT be valid for more than a week old | can define a new variable holding one week ago
-	local conn = assert( sql.connect(string.format('/db/%s.db', w.week)) )
-	if conn.count('updates', string.format(clause, vers)) > 0 then
-	    push( conn, clause, ret )
-	end
+	push( w.week, vers, ret )
 	vers = 0
     end
 
-    local conn = assert( sql.connect(string.format('/db/%s.db', week)) )
-    if conn.count('updates', string.format(clause, vers)) > 0 then
-	push( conn, clause, ret )
-    end
-    return ret
+    vers = push( week, vers, ret )
+
+    ret.VERS = {store='VERS', week=week, vers=vers}
+
+    ret = fd.reduce(fd.keys( ret ), fd.map( hd.asJSON ), fd.into, {})
+
+    return string.format('Content-Type: text/plain; charset=utf-8\r\n\r\n[%s]\n', table.concat(ret, ', '))
 end
 
 aux( records )
