dc7d72f5b623eca1718f8b19ff9bda7b944a84a6
Charlie Root <root@ferreBSD.bancomer.com>
2018-02-11 14:10:22 -0600

    lsdf sdf sd kj

---
+++ caja/facturar.lua
@@ -0,0 +1,57 @@
+#!/usr/local/bin/lua
+
+local aux = require'ferre.aux'
+local lpr = require'ferre.bixolon'
+local tkt = require'ferre.ticket'
+local ex = require'ferre.extras'
+local precio = require'ferre.precio'
+
+local fd = require'carlos.fold'
+
+--[[ NEED TO BE REWRITTEN XXX
+local ventas = {VENTA=true, FACTURA=true, CREDITO=true}
+
+local function ventap(s)
+    return (ventas[s] and 'ventas' or 'tickets')
+end
+--]]
+
+local function tickets(q)
+    local uid = q.uid
+    local tag = q.tag or ''
+    local y,m,d = uid:match'^(%d+)-(%d+)-(%d+)'
+    local week = ex.asweek(os.time{year=y, month=m, day=d})
+    local conn = assert(ex.dbconn(week), 'Error while connecting to DB ' .. week)
+    local QRY = 'SELECT * FROM %s WHERE uid LIKE %q'
+
+    assert(conn.exec'ATTACH DATABASE "/db/ferre.db" AS FR')
+
+    local function fetch()
+	local p = 'NaP'
+
+        local total = 0
+	local function suma(w)
+	    total = total + w.totalCents
+	    return w
+	end
+	local function bruto(w)
+	    w.prc = w.prc / 1.16
+	    w.subTotal = string.format('%.2f', w.totalCents/116)
+	    return w
+	end
+
+	-- uid REQUIRED to get DATE & TIME
+	local ret = {uid=uid, person=p, tag=tag}
+	ret.datos = fd.reduce( conn.query(string.format(QRY, 'tickets', uid)), fd.map(precio(conn)), fd.map(suma), fd.map(bruto), fd.into, {} ) -- ventap(tag) : DBase to connect
+	ret.total = string.format('%.2f', total/116) -- BRUTO
+
+	return ret
+    end
+
+    lpr( tkt( fetch() ) )
+--]]
+
+    return 'OK'
+end
+
+aux( tickets )
